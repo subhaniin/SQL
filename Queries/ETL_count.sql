54acb557b1140af65f041af5cad87019a3f218a0
subhaniin <70527643+subhaniin@users.noreply.github.com>
2025-03-20 16:30:40 +0530

    Update ETL_count.sql
    added comment

--- Queries/ETL_count.sql
+++ Queries/ETL_count.sql
@@ -1,3 +1,7 @@
+SELECT COUNT(*) FROM customers AS source_count,
+SELECT COUNT(*) FROM orders AS target_count;
+
+--Single table
 SELECT 
     (SELECT COUNT(*) FROM customers) AS source_count,
     (SELECT COUNT(*) FROM orders) AS target_count;
