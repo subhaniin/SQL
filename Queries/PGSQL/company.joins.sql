cf41b25d423273062b988a2ce1d383335b6665f1
subhaniin <70527643+subhaniin@users.noreply.github.com>
2025-10-18 16:45:03 +0530

    cc

--- Queries/PGSQL/company.joins.sql
+++ Queries/PGSQL/company.joins.sql
@@ -1,1 +1,5 @@
-select e.emp_id,e.emp_name, d.dept_id from employees e join departments d on e.dept_id=d.dept_id;
+select e.emp_id,e.emp_name, d.dept_id from employees e 
+join departments d on e.dept_id=d.dept_id;
+
+select e.emp_name,ep.first_name,ep.last_name from employees e
+join employee_pd ep on e.emp_id = ep.emp_id;
--- Queries/PGSQL/employee_pd.sql
+++ Queries/PGSQL/employee_pd.sql
@@ -4,8 +4,7 @@ create table employee_PD(emp_id int primary key,first_name varchar(25), last_nam
 insert into employee_PD(emp_id,first_name,last_name)
 select emp_id, first_name,last_name from employees;
 
-select e.first_name,ep.first_name from employees e
-join employee_pd ep on e.emp_id = ep.emp_id;
+
 
 alter table employees
 drop first_name,
