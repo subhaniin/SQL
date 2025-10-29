dab45a58679be571eadd9920e6762ace95bcde2b
subhaniin <70527643+subhaniin@users.noreply.github.com>
2025-10-29 17:27:02 +0530

    cc

--- Queries/PGSQL/CTE.sql
+++ Queries/PGSQL/CTE.sql
@@ -6,3 +6,25 @@ WITH dept_avg AS (
 SELECT dept_id, avg_salary
 FROM dept_avg
 WHERE avg_salary > 70000;
+
+WITH RECURSIVE numbers AS (
+    -- Anchor: start with 1
+    SELECT 1 AS num
+
+    UNION ALL
+
+    -- Recursive: add 1 each time
+    SELECT num + 1
+    FROM numbers
+    WHERE num < 10
+)
+SELECT * FROM numbers;
+
+WITH RECURSIVE clanker AS (
+    SELECT 'CLANK' AS sound, 1 AS num
+    UNION ALL
+    SELECT 'CLANK', num + 1
+    FROM clanker
+    WHERE num < 10
+)
+SELECT * FROM clanker;
