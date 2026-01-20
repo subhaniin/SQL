b6f60f492dc47dbb3be50091832e6d9ca659b5ee
subhaniin <70527643+subhaniin@users.noreply.github.com>
2026-01-21 02:12:54 +0530

    ic

---
+++ Queries/PGSQL/Greaterthan_dept_avg.sql
@@ -0,0 +1,43 @@
+select * from
+(select emp_name,dept_id,salary, avg(salary) over(partition by dept_id) as avg from employees) 
+where salary>avg;
+
+select 
+	e.emp_name,
+	e.dept_id,
+	e.salary,
+	d.dept_avg 
+from employees e
+join(
+	select dept_id,avg(salary)as dept_avg 
+	from employees 
+	group by dept_id 
+)d
+on e.dept_id = d.dept_id
+where e.salary > d.dept_avg;
+
+
+SELECT emp_name, dept_id, salary
+FROM employees;
+--QUALIFY salary > AVG(salary) OVER (PARTITION BY dept_id); qualify only works Snowflake, BigQuer, Teradat, Redshift (partial)
+select * 
+from(
+	select
+		e.emp_name,e.salary,e.dept_id,d.dept_name,avg(e.salary) over(partition by e.dept_id) as dept_avg 
+	from 
+		employees e
+	join 
+	departments d on e.dept_id = d.dept_id)
+where 
+	salary>dept_avg;
+--------------------------------------------------------	
+select * 
+from(
+	select e.dept_id,d.dept_name,e.salary,
+	dense_rank()over(partition by e.dept_id order by e.salary desc) 
+	as salary_rank,e.hire_date,avg(e.salary) over (partition by e.dept_id) as dept_avg from employees e
+	join departments d
+	on e.dept_id = d.dept_id) 
+where salary_rank <3 and hire_date>'2021-01-01' and dept_avg >6000;
+--------------------------------------------------------
+select * from employees where hire_date > '2010-01-01'
